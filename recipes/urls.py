3aa04edcaf77c6b9314f7bc5ec9ba6d1ad6a00fc
Christopher Tranter <mr_crock@hotmail.co.uk>
2024-07-22 14:04:27 +0000

    included urls for edit delete functinality

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.Recipelist.as_view(), name='home'),
     path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
-    path('add_recipe', views.AddRecipe.as_view(), name='add_recipe'),           
+    path('add_recipe', views.AddRecipe.as_view(), name='add_recipe'),
+    path('update_recipe/<int:pk>',views.UpdateRecipe.as_view(), name='update_recipe'),
+    path('delete_recipe/<int:recipe_id>',views.delete_recipe, name='delete_recipe'),                
 ]
