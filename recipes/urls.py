5fc55b64f0092b2e3a13cd891f2847e11580e475
Christopher Tranter <mr_crock@hotmail.co.uk>
2024-07-24 09:43:23 +0000

    Create url for my recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add_recipe', views.AddRecipe.as_view(), name='add_recipe'),
     path('update_recipe/<int:pk>',views.UpdateRecipe.as_view(), name='update_recipe'),
     path('delete_recipe/<int:recipe_id>',views.delete_recipe, name='delete_recipe'),                
+    path('my_recipes',views.MyRecipes.as_view(), name='my_recipes'),                
 ]
