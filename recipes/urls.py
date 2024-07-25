7ddbe40fc7c838673f5bd8271a258cbc1eca7512
Christopher Tranter <mr_crock@hotmail.co.uk>
2024-07-25 14:55:18 +0000

    validated views.py add validated images

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path("", views.Recipelist.as_view(), name="home"),
     path("<slug:slug>/", views.recipe_detail, name="recipe_detail"),
     path("add_recipe", views.AddRecipe.as_view(), name="add_recipe"),
-    path("update_recipe/<int:pk>", views.UpdateRecipe.as_view(), name="update_recipe"),
-    path("delete_recipe/<int:recipe_id>", views.delete_recipe, name="delete_recipe"),
+    path("update_recipe/<int:pk>", views.UpdateRecipe.as_view(),
+    name="update_recipe"),
+    path("delete_recipe/<int:recipe_id>", views.delete_recipe,
+    name="delete_recipe"),
     path("my_recipes", views.MyRecipes.as_view(), name="my_recipes"),
 ]
